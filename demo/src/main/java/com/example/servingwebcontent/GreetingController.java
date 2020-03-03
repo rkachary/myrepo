a747122a8cb7e090b1c5ebd1827eeb39008dfb2a
rkachary <34763359+rkachary@users.noreply.github.com>
2020-03-03 15:22:12 +0530

    Added the comment.

--- demo/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ demo/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+	/**
+	 * 
+	 * @param name
+	 * @param model
+	 * @return
+	 */
 		@GetMapping("/greeting")
 		public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 			model.addAttribute("name", name);
